0522264f852c799f9089e5ecdec3d53272bcfe99
Aurelia Beck <aurelia.beck@liganova.com>
2024-12-09 14:27:35 +0100

    add red button component

---
+++ app/components/Button.js
@@ -0,0 +1,14 @@
+// components/Button.js
+import styles from './Button.module.css';
+
+export default function Button({ children, onClick, disabled }) {
+  return (
+    <button
+      className={styles.button}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
