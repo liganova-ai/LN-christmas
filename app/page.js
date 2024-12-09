e67364577c3c036141e52755f48169f2be202d18
Aurelia Beck <aurelia.beck@liganova.com>
2024-12-09 17:41:11 +0100

    add start page styling

--- app/page.js
+++ app/page.js
@@ -1,13 +1,30 @@
-"use client"
+"use client";
+
 import { useRouter } from 'next/navigation';
+import styles from './homepage.module.css';
+import Button from './components/Button';
 
 export default function HomePage() {
   const router = useRouter();
 
   return (
-    <div className="welcome-screen">
-      <h1>Welcome to the AI Photo App</h1>
-      <button onClick={() => router.push('/upload')}>Start</button>
+    <div className={styles.homePage}>
+      <header className={styles.header}>
+        <img src="/logo-left.png" alt="Logo" className={styles.logo} /> {/* Adjust the logo path */}
+      </header>
+
+      <div className={styles.content}> 
+      <div className={styles.leftSection}>
+        <h1 className={styles.heading}>Already dreaming about whats next?</h1>
+        <p className={styles.copyText}>
+          Upload a selfie, choose your next travel destination and let the AI do the magic.
+        </p>
+        <div className={styles.buttonContainer}>
+        <Button onClick={() => router.push('/upload')}>Start experience</Button>
+        
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
