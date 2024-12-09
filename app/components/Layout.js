2ae1c5b13f12d492b6dcb119651214759d0fb503
Aurelia Beck <aurelia.beck@liganova.com>
2024-12-09 17:41:37 +0100

    change styling of components for all pages

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,26 +1,24 @@
-// components/Layout.js
 import Image from 'next/image';
 import styles from './Layout.module.css';
-import logo from '../../public/logo-left.png'; // Ensure this is the path to your uploaded logo
+import logo from '../../public/logo-left.png';
 
-export default function Layout({ heading, copyText, children, isFirstPage }) {
+export default function Layout({ heading, copyText, children, rightContent }) {
   return (
-    <div className={`${styles.layout} ${isFirstPage ? styles.firstPage : ''}`}>
-      {/* Logo */}
+    <div className={styles.layout}>
       <header className={styles.header}>
         <Image src={logo} alt="Logo" className={styles.logo} />
       </header>
 
-      {/* Main Content */}
       <div className={styles.content}>
         {/* Left Section */}
         <div className={styles.leftSection}>
           <h1 className={styles.heading}>{heading}</h1>
-          <p className={styles.copyText}>{copyText}</p>
+          {copyText && <p className={styles.copyText}>{copyText}</p>}
+          <div className={styles.buttonContainer}>{children}</div>
         </div>
 
         {/* Right Section */}
-        <div className={styles.rightSection}>{children}</div>
+        <div className={styles.rightSection}>{rightContent}</div>
       </div>
     </div>
   );
