199fb076fc1c1ff74e6dfa0dd27c749fb64a8701
Aurelia Beck <aurelia.beck@liganova.com>
2024-12-15 11:46:05 +0100

    add font files and next js font config

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,57 @@
-// app/layout.js
-import Layout from './components/Layout';  // Adjust the path if needed
+import styles from './globals.css'; 
+import localFont from 'next/font/local'; 
+
+
+const luxury = localFont({
+  src: [
+    {
+      path: '/fonts/Luxury-Diamond.otf',
+      weight: '300',
+      style: 'diamond',
+    },
+    {
+      path: '/fonts/Luxury-Gold.otf',
+      weight: '200',
+      style: 'golden',
+    },
+    {
+      path: '/fonts/Luxury-Platinum.otf',
+      weight: '700',
+      style: 'platinum',
+    },
+  ],
+  variable: '--font-luxury',  // css variable name 
+});
+
+const sabon = localFont({
+  src: [
+    {
+      path: '/fonts/SabonLTStd-Roman.otf',
+      weight: '300',
+      style: 'Roman',
+    },
+    {
+      path: '/fonts/SabonLTStd-Italic.otf',
+      weight: '500',
+      style: 'Italic',
+    },
+    {
+      path: '/fonts/SabonLTStd-BoldItalic.otf',
+      weight: '700',
+      style: 'BoldItalic',
+    },
+    {
+      path: '/fonts/SabonLTStd-Bold.otf',
+      weight: '800',
+      style: 'Bold',
+    },
+  ],
+  variable: '--font-sabon',  // css variable name
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${luxury.variable} ${sabon.variable}`}>
       <body>
         {children}
       </body>
