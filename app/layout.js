313f6988fe6d11e1badbc45368aba53d4bfd8cc4
Aurelia Beck <aurelia.beck@liganova.com>
2024-12-15 13:51:18 +0100

    finish fonts and their next js configurations

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,22 @@
 import styles from './globals.css'; 
 import localFont from 'next/font/local'; 
+import {Freehand} from 'next/font/google';
 
+const freehand = Freehand({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const futura = localFont({
+  src: [
+    {
+    path: '/fonts/FuturaStd-Medium.otf',
+    weight: '500',
+    style: 'medium',
+  }
+], variable: '--font-futura'
+})
 
 const luxury = localFont({
   src: [
@@ -51,7 +67,7 @@ const sabon = localFont({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${luxury.variable} ${sabon.variable}`}>
+    <html lang="en" className={`${luxury.variable} ${sabon.variable} ${futura.variable} ${freehand.className}`}>
       <body>
         {children}
       </body>
