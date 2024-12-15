b49e1e66b310b795e993aab44179a2016efecc5a
Aurelia Beck <aurelia.beck@liganova.com>
2024-12-15 22:00:12 +0100

    add color props for logo component

--- app/components/logo.js
+++ app/components/logo.js
@@ -4,9 +4,17 @@ import React from 'react';
 import styles from './logo.module.css';
 import PropTypes from 'prop-types';
 
+const colorMap = {
+  yellow: '#F3DF6E',
+  red: '#C32F33',
+  pink: '#DE75A5',
+};
+
 const Logo = ({ color }) => {
+  const hexColor = colorMap[color] || '#000'; // Fallback to black if color is invalid
+
   return (
-    <div className={styles.logo} style={{ color }}>
+    <div className={styles.logo} style={{ color: hexColor }}>
       LIGANOVA
     </div>
   );
