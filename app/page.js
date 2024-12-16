9742b76d423956f30dc144351ad9e1cf64f03e02
Aurelia Beck <aurelia.beck@liganova.com>
2024-12-16 16:13:26 +0100

    add svg to start page and change styling

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import styles from './page.module.css';
 import Button from './components/Button';
 import Logo from './components/logo';
+import Image from 'next/image';
 
 export default function HomePage() {
   const router = useRouter();
@@ -15,7 +16,17 @@ export default function HomePage() {
       </header>
       <div className={styles.content}>
         <div className={styles.textContainer}>
-        <h1 className={styles.heading}>CIAO NATALE</h1>
+            <div className={styles.headingContainer}>
+          <div className={styles.imageWrapper}>
+            <Image 
+              src="/Title_Graphic.svg" 
+              alt="Title Graphic" 
+              className={styles.headingImage} 
+              fill 
+              priority 
+            />
+          </div>
+        </div>
         <p className={styles.copyText}>
           Dive into the world of AI-generated visuals with our very italian 80s image generator.
         </p>
