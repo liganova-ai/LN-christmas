2f66ebb13a0880539c1710a6a4794a6a8ff23ab6
Aurelia Beck <aurelia.beck@liganova.com>
2024-12-17 16:41:48 +0100

    first version of result page

--- app/result/page.js
+++ app/result/page.js
@@ -1,15 +1,17 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import Layout from '../components/Layout';
-import Button from '../components/Button';
 import Image from 'next/image';
 import styles from './result.module.css';
+import Logo from '../components/logo';
+import html2canvas from 'html2canvas';
 
 export default function ResultPage() {
   const router = useRouter();
   const [prediction, setPrediction] = useState(null);
+  const imageRef = useRef(null);
 
   useEffect(() => {
     const storedPrediction = localStorage.getItem('predictionResult');
@@ -27,28 +29,57 @@ export default function ResultPage() {
     router.push('/');
   };
 
+  const handleDownload = async () => {
+    if (imageRef.current) {
+      const canvas = await html2canvas(imageRef.current);
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL('image/png');
+      link.download = 'xmas24-ai-timemachine.png';
+      link.click();
+    }
+  };
+
   return (
-    <Layout
-      heading="CIAO BELLO/A"
-      copyText=""
-      middleContent={
-        prediction && (
-          <div className={styles.imageWrapper}>
-            <Image
-              src={prediction.output[prediction.output.length - 1]}
-              alt="Generated Image"
-              width={378}
-              height={486}
-              objectFit="cover"
-            />
-          </div>
-        )
-      }
-    >
-      <div className={styles.buttonContainer}>
-        <Button>Download</Button>
-        <Button onClick={handleRepeat}>Repeat</Button>
-      </div>
-    </Layout>
+    <div className={styles.resultPage}>
+      <header className={styles.header}>
+        <Logo color="pink" />
+      </header>
+
+      <Layout
+        heading="CIAO BELLO"
+        headingColor="#DE75A5"
+        copyText=""
+        middleContent={
+          prediction && (
+            <div className={styles.imageWrapper} ref={imageRef}>
+              <Image
+                src={prediction.output[prediction.output.length - 1]}
+                alt="Generated Image"
+                width={600}
+                height={800}
+                objectFit="cover"
+                className={styles.generatedImage}
+              />
+              <Image
+                src="/Photo_Frame_Overlay.svg"
+                alt="Overlay"
+                layout="fill"
+                objectFit="contain"
+                className={styles.overlay}
+              />
+            </div>
+          )
+        }
+      >
+        <div className={styles.buttonContainer}>
+          <button className={styles.customButton} onClick={handleDownload}>
+            Download
+          </button>
+          <button className={styles.customButton} onClick={handleRepeat}>
+            Repeat
+          </button>
+        </div>
+      </Layout>
+    </div>
   );
 }
